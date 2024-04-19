39d635261b17de65452d80c64c7edd7e733b746e
Camila Muniz <160981013+camilamuniz@users.noreply.github.com>
2024-04-19 01:20:17 +0000

    jabuticaba

--- app/src/main/java/application/Main.java
+++ app/src/main/java/application/Main.java
@@ -1,8 +1,27 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!!!");
+        int[] numeros = new int[5];
+
+        for(int con=0; con<numeros.length; con++) {
+            try {
+                System.out.println("Digite um número inteiro:");
+                Scanner sc = new Scanner(System.in);
+                numeros[con] = sc.nextInt();
+                    }   catch (InputMismatchException ex) {
+                System.out.println("tipo de dado invalido");
+                con--;
+            }
+        }
+
+        System.out.println("Valores Válidos: ");
+        for(int val : numeros) {
+            System.out.print(val + " ");
+        }
     }
 }
 
